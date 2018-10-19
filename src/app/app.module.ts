53a5a18a6ba73d38e0ea055b7a00a80a26879c9c
Aude <aude_kreiter@hotmail.com>
2018-10-19 14:22:09 +0200

    aude

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BiblioComponent } from './biblio/biblio.component';
+import { ListComponent } from './list/list.component';
+import { PlaylistComponent } from './playlist/playlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BiblioComponent,
+    ListComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule
