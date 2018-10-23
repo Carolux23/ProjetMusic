0990595dff1899006d1b911ce28d9d9fdaafd986
Aude <aude_kreiter@hotmail.com>
2018-10-23 10:22:28 +0200

    creation component inside du comonent genre

---
+++ src/app/in-genre/in-genre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-in-genre',
+  templateUrl: './in-genre.component.html',
+  styleUrls: ['./in-genre.component.css']
+})
+export class InGenreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
